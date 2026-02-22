f05e4b83a9bdddfc12c9edad45aca7fbb8d0264f
Ajeet Yadav <ajeetyadav164@gmail.com>
2026-02-22 23:20:14 +0530

    Initial Commit

---
+++ TapHouse/TapHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapHouseApp.swift
+//  TapHouse
+//
+//  Created by Ajeet Yadav on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TapHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
